b485e509691082b4699dd70826fa364ebf5e818f
Cleofas Pereira <30829268+cleofase@users.noreply.github.com>
2017-12-04 01:36:10 -0200

    JellyFly2D
    JellyFly2D

---
+++ JellyFly2D/Assets/Scripts/BgScroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgScroll : MonoBehaviour {
+	private Material material;
+	private Vector2 offSet;
+	private float posicao = 0;
+	public float velocidade = 0;
+	// Use this for initialization
+	void Start () {
+		material = GetComponent<Renderer> ().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		posicao += velocidade;
+		material.mainTextureOffset = new Vector2(posicao,0);
+	}
+}
