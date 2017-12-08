4be16840809d77a9cc10740911a656258013ba89
Haikal Rios <haikalrios@gmail.com>
2017-12-08 11:45:32 -0200

    teste

---
+++ JellyFly2D/Assets/Scripts/menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void NewGame(){
+		SceneManager.LoadScene("Fase01");
+	}
+
+	public void HomeMenu(){
+		SceneManager.LoadScene("Menu");
+	}
+
+
+	public void ExitGame(){
+		Application.Quit();
+	}
+}
